33c385117f7a33049b567d81a4c620d79862183d
KoatKoetl <stas20027@outlook.com>
2024-01-05 00:34:13 +0200

    Add loaders for Avif and Webp images

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,14 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.avif$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.webp$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
