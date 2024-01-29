aad09a7ac614e210d04b4037622ea5c464f0b7b0
KoatKoetl <stas20027@outlook.com>
2024-01-29 22:31:04 +0200

    Delete homeland section

--- src/JS/App.js
+++ src/JS/App.js
@@ -1,14 +1,10 @@
-import enable_HeaderBurgerMenu from './Burger-menu.js';
-import render_CountrySectionData from './Country-section.js';
-import enable_InfoButton from './Button.js';
-import { enable_SkillSection } from './Skills.js';
-import render_ProjectsSection from './Projects.js';
 import { enable_AllAnimations } from './Animations.js';
+import enable_HeaderBurgerMenu from './Burger-menu.js';
+import render_ProjectsSection from './Projects.js';
+import { enable_SkillSection } from './Skills.js';
 
 export default function enableApp() {
   enable_HeaderBurgerMenu();
-  render_CountrySectionData();
-  enable_InfoButton();
   enable_SkillSection();
   render_ProjectsSection();
   enable_AllAnimations();
