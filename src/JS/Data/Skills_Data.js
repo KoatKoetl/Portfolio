16ffe997f9d4820cb9b58c8e0143767a1eefdf43
KoatKoetl <stas20027@outlook.com>
2024-02-27 01:47:15 +0200

    Fix: skill image

--- src/JS/Data/Skills_Data.js
+++ src/JS/Data/Skills_Data.js
@@ -4,13 +4,13 @@ const data_AllSkills = [
   { name: 'CSS3', imageURL: 'assets/css-3-svgrepo-com.svg' },
   { name: 'JavaScript', imageURL: 'assets/javascript-svgrepo-com.svg' },
   { name: 'TailwindCSS', imageURL: 'assets/tailwind-svgrepo-com.svg' },
-  { name: 'React', imageURL: 'assets/react-svgrepo-com.svg' },
   { name: 'Jest', imageURL: 'assets/jest-svgrepo-com.svg' },
   { name: 'Webpack', imageURL: 'assets/webpack-svgrepo-com.svg' },
   { name: 'Babel', imageURL: 'assets/babel-svgrepo-com.svg' },
   { name: 'ESLint', imageURL: 'assets/eslint-svgrepo-com.svg' },
   { name: 'Git', imageURL: 'assets/git-svgrepo-com.svg' },
   { name: 'NPM', imageURL: 'assets/npm-svgrepo-com.svg' },
+  { name: 'React', imageURL: 'assets/react-svgrepo-com.svg' },
 ];
 
 export default data_AllSkills;
